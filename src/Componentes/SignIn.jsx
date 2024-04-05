5aade0b58753cafe3c354a90ffd8a979863cbc42
yassinkr <kerraiyassin@gmail.com>
2024-04-06 00:01:35 +0100

    fixed margin error

--- src/Componentes/SignIn.jsx
+++ src/Componentes/SignIn.jsx
@@ -8,7 +8,7 @@ const SignIn = () => {
     <div className="w-full h-screen overflow-hidden flex flex-col justify-center items-center" id="SignIn">
         <Navbar />
         <div className="flex justify-center w-full h-screen pt-48 ">
-            <div className="flex flex-col justify-start items-start max-w-[400px] w-full">
+            <div className="flex flex-col justify-start items-start max-w-[400px] w-full mx-5">
                 <h1 className="text-2xl font-Bodoni  mb-5 mx-auto">Se connecter / S’inscrire</h1>
                 <label htmlFor="email" className="mb-2 text-herBlack text-[16px] ">adresse e-mail ou numéro</label>
                 <input type="text" className="border-[1px] border-black p-2 mb-3 w-full"/>
--- src/Componentes/SignUp.jsx
+++ src/Componentes/SignUp.jsx
@@ -8,7 +8,7 @@ const SignUp = () => {
     <div className="w-full h-screen overflow-hidden flex flex-col justify-center items-center" id="SignIn">
         <Navbar />
         <div className="flex justify-center w-full h-screen pt-48 ">
-            <div className="flex flex-col justify-start items-start max-w-[300px] w-full">
+            <div className="flex flex-col justify-start items-start max-w-[300px] w-full mx-5">
                 <h1 className="text-2xl font-Bodoni  mb-5 mx-auto">S’inscrire</h1>
                 <label htmlFor="brand" className="mb-2 text-herBlack text-[16px] ">nom du brand :</label>
                 <input type="text" className="border-[1px] border-black p-2 mb-3 w-full"/>
